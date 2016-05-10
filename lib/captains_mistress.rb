e2a4e7e5a0257637ffe280885b15a6915b8fe782
Ian James <ian@ioffer.com>
2016-05-10 11:58:01 -0700

    Fixes for rubocop

--- lib/captains_mistress.rb
+++ lib/captains_mistress.rb
@@ -1,5 +1,6 @@
-require "captains_mistress/version"
+require 'captains_mistress/version'
 
+# A simple game of Captain's Mistress.
 module CaptainsMistress
   # Your code goes here...
 end
--- lib/captains_mistress/version.rb
+++ lib/captains_mistress/version.rb
@@ -1,3 +1,3 @@
 module CaptainsMistress
-  VERSION = "0.1.0"
+  VERSION = '0.1.0'.freeze
 end
